731cbae5b3d7c391ff997434da98c407818982db
stevensotelo <stevenbetancurt@hotmail.com>
2021-03-15 11:21:10 -0500

    fixed calculate distance in risk file and also add a method to calculate risk direct

--- src/risk_calculator/risk.py
+++ src/risk_calculator/risk.py
@@ -4,6 +4,12 @@ import rasterio as rio
 import geopandas as gpd
 import fiona
 from shapely.ops import nearest_points
+import multiprocessing as mp
+import numpy as np
+import pandas as pd
+import math
+from fiona.crs import from_epsg
+
 
 # Method that calculate risk of deforestation by plot
 # (string) inputs: Path where inputs files should be located
@@ -41,7 +47,7 @@ def deforestation_plot(inputs, years, types_analysis, pixel_size):
                     print("Parsing to Geopandas")
                     gdf  = gpd.GeoDataFrame.from_features(stats,crs = de.crs.data)
                     gdf["def_area"] = gdf["count"] * (pixel_size*pixel_size)
-                    gdf["def_prop"] = (gdf["area"] / gdf["def_area"]) * 100
+                    gdf["def_prop"] = (gdf["def_area"] / gdf["area"]) * 100
 
                     f_folder = os.path.join(outputs_folder, ta)
                     if not os.path.exists(f_folder):
@@ -65,7 +71,16 @@ def near_distance(point, pts):
     distance = point.distance(nearest)
     return distance
 
-#
+
+def neighbour_distance(gdf_chunk, pts):    
+    for index, row in gdf_chunk.iterrows(): # Iterate over the chunk
+        gdf_chunk.at[index,'distance'] = near_distance(row.centroid, pts)
+    return gdf_chunk
+
+# Method which calculate the distance between centroid of buffer of each plot with the near deforestation point
+# (string) inputs: Path where inputs files should be located
+# (array) years: Array of ints with all years that should be processed
+# (array) types_analysis: Array of strings with the types of
 def distance_plot(inputs, years, types_analysis):
     in_areas_root = os.path.join(inputs,"plot","fixed","def")
     in_def_root = os.path.join(inputs,"def","fixed")
@@ -74,12 +89,17 @@ def distance_plot(inputs, years, types_analysis):
     if not os.path.exists(outputs_folder):
         os.mkdir(outputs_folder)
     
-    # Loop for years which want to be analyzed
-    for y in years:    
-        print("Processing year: " + str(y))
+    # CPUS to use
+    cpus = mp.cpu_count() - 1 
+    pool = mp.Pool(processes=cpus)
+
+    # Loop for type of analysis, they can be detail or summary
+    for ta in types_analysis:
+        print("Processing type: " + ta)
         
-        # Loop for type of analysis, they can be detail or summary
-        for ta in types_analysis:
+        # Loop for years which want to be analyzed
+        for y in years:    
+            print("Processing year: " + str(y))
             
             file_areas = os.path.join(in_areas_root,ta,str(y),"areas.shp")
             print("Opening areas shp: " + file_areas)    
@@ -94,9 +114,14 @@ def distance_plot(inputs, years, types_analysis):
             shp_areas['centroid'] = shp_areas.centroid
             # Extracting geometries of deforestation
             pts = shp_def.geometry.unary_union
-            
-            print("Calculating distances")
-            shp_areas['def_distance'] = shp_areas.apply(lambda row: near_distance(row.centroid, pts), axis=1)
+
+            print("Starting process parallel")
+            shp_areas_chunks = np.array_split(shp_areas, cpus)
+            chunk_processes = [pool.apply_async(neighbour_distance, args=(chunk, pts)) for chunk in shp_areas_chunks]
+            shp_areas_results = [chunk.get() for chunk in chunk_processes]
+            print("Joining results")
+            shp_areas = gpd.GeoDataFrame(pd.concat(shp_areas_results), crs=shp_areas.crs)
+            shp_areas = shp_areas.drop('centroid', 1)
             
             f_folder = os.path.join(outputs_folder, ta)
             if not os.path.exists(f_folder):
@@ -108,8 +133,86 @@ def distance_plot(inputs, years, types_analysis):
             output_file = os.path.join(f_folder, "areas_distance.shp")
             print("Saving: " + output_file)
             shp_areas.to_file(output_file)
-            
-            
+
+
+def calculate_risk_direct(gdf_chunk, param):    
+    for index, row in gdf_chunk.iterrows(): # Iterate over the chunk
+        # Calculate DP
+        def_prop =  (row.def_area / row.area) * 100.0
+        gdf_chunk.at[index,'def_prop'] = def_prop        
+        dp = 0
+        if def_prop > 2.0:
+            dp = 4
+        elif (def_prop > 1.0) & (def_prop <= 2.0): 
+            dp = 3
+        elif (def_prop > .5) & (def_prop <= 1.0): 
+            dp = 2
+        elif (def_prop > 0.0) & (def_prop <= .5): 
+            dp = 1
+        gdf_chunk.at[index,'dp'] = dp
+        # Calculate DD
+        dd = 0
+        if row.distance <= 1000.0:
+            dd = 4
+        elif (row.distance > 1000.0) & (row.distance <= 2000.0): 
+            dp = 3
+        elif (row.distance > 2000.0) & (row.distance <= 3000.0): 
+            dp = 2
+        elif (row.distance > 3000.0) & (row.distance <= 5000.0): 
+            dp = 1
+        gdf_chunk.at[index,'dd'] = dd
+        gdf_chunk.at[index,'rd'] = math.ceil((dd * 0.5) + (dp * 0.5))
+
+    return gdf_chunk
+
+def risk_direct(inputs, years, types_analysis, dst_crs):
+    in_dis_root = os.path.join(inputs,"plot","fixed","dis")
+    # Create output folder
+    outputs_folder = os.path.join(inputs,"plot","fixed","risk")
+    if not os.path.exists(outputs_folder):
+        os.mkdir(outputs_folder)
+    
+    # CPUS to use
+    cpus = mp.cpu_count() - 1 
+    pool = mp.Pool(processes=cpus)
+
+    # Loop for type of analysis, they can be detail or summary
+    for ta in types_analysis:
+        print("Processing type: " + ta)
+        
+        # Loop for years which want to be analyzed
+        for y in years:    
+            print("Processing year: " + str(y))
+
+            file_dis = os.path.join(in_dis_root,ta,str(y),"areas_distance.shp")
+            print("Opening areas shp: " + file_dis)    
+            shp_dis = gpd.read_file(file_dis)
+
+            print("Reprojecting to: " + str(from_epsg(dst_crs)))
+            shp_dis = shp_dis.to_crs(crs = from_epsg(dst_crs))
+
+            print("Starting process parallel")
+            shp_dis_chunks = np.array_split(shp_dis, cpus)            
+            chunk_processes = [pool.apply_async(calculate_risk_direct, args=(chunk, "")) for chunk in shp_dis_chunks]
+            shp_dis_results = [chunk.get() for chunk in chunk_processes]
+            print("Joining results")
+            shp_dis = gpd.GeoDataFrame(pd.concat(shp_dis_results), crs=shp_dis.crs)
+
+            f_folder = os.path.join(outputs_folder, ta)
+            if not os.path.exists(f_folder):
+                os.mkdir(f_folder)
+                        
+            f_folder = os.path.join(f_folder, str(y))
+            if not os.path.exists(f_folder):
+                os.mkdir(f_folder)
+            output_file = os.path.join(f_folder, "rd.shp")
+            print("Saving: " + output_file)
+            shp_dis.to_file(output_file)
+
+
+
+
+
             
             
 
