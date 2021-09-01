07f8f15e76db67e31dc353fbdd43cb6204786962
alejomarulanda <alejandromarulanda@live.com>
2021-09-01 09:33:19 -0500

    Actualice el Home de GanaBosques

--- src/app/fauno/src/components/footer/Footer.js
+++ src/app/fauno/src/components/footer/Footer.js
@@ -4,12 +4,8 @@ class Footer extends Component {
 
     render() {
         return (
-            <footer className="container">
-                <p className="float-right"></p>
-                <p>
-                    &copy; 2021 Alliance Bioversity International - CIAT
-                    <a href="#">Política de Privacidad</a> <a href="#">Términos y condiciones</a>
-                </p>
+            <footer >
+                    &copy; 2021 Alliance Bioversity International - CIAT - <a href="#">Política de Privacidad</a> - <a href="#">Términos y condiciones</a>
             </footer>
         );
     }
